7017262eff80839857c8986f37fcd7bc43d5feb2
Jason Townsend <jtownsen@netapp.com>
2023-06-29 18:08:46 -0500

    fixing final touches, getting _diag/_error to work

--- collect_nfsv4_statatistics.sh
+++ collect_nfsv4_statatistics.sh
@@ -1,31 +1,32 @@
 #!/bin/bash
 
 #Set the IP address of the node management server
-NODE_MANAGEMENT="NODE_MGMT_IP"  # NODE_MANAGEMENT="10.2.1.3"
+NODE_MANAGEMENT="NODE-MGMT-IP"  # NODE_MANAGEMENT="10.2.1.3"
 
 #Set the username to user during authentication (Ideal to use pubkey auth)
 USERNAME="admin"
 
 #Create a directory to store the statistics
-mkdir -p statistics
+mkdir -p statistics/$NODE_MANAGEMENT
 
 
-#Collect the statistics for NFSv4
-for TYPE in v4 v4_1
+#Collect the statistics for NFSv4 and NFSv4_1
+for version in nfsv4 nfsv4_1
 do
-ssh $USERNAME@$NODE_MANAGEMENT "set d -c off; rows 0; date; statistics show -object nfs$TYPE -raw" >> statistics/nfs.txt
-ssh $USERNAME@$NODE_MANAGEMENT "set d -c off; rows 0; date; statistics show -object nfs{$TYPE}_diag -raw" >> statistics/nfs.txt
-ssh $USERNAME@$NODE_MANAGEMENT "set d -c off; rows 0; date; statistics show -object nfs{$TYPE}_error -raw" >> statistics/nfs.txt
+    for TYPE in "" "_diag" "_error"
+    do
+        ssh $USERNAME@$NODE_MANAGEMENT "set d -c off; rows 0; date; statistics show -object $version$TYPE -raw" >> statistics/$NODE_MANAGEMENT/nfs.txt
+    done
 done
 
 #Collect the statistics for spinnp
 for TYPE in spinnp_error spinhi spinnp
 do
-ssh $USERNAME@$NODE_MANAGEMENT "set d -c off; rows 0; date; statistics show -object $TYPE -raw" >> statistics/spin.txt
+ssh $USERNAME@$NODE_MANAGEMENT "set d -c off; rows 0; date; statistics show -object $TYPE -raw" >> statistics/$NODE_MANAGEMENT/spin.txt
 done
 
 #Collect the statistics for lmgr_ng
-ssh $USERNAME@$NODE_MANAGEMENT "set d -c off; rows 0; date; statistics show -object lmgr_ng -counter files|hosts|owners|locks|*max -raw" >> statistics/locks.txt
+ssh $USERNAME@$NODE_MANAGEMENT "set d -c off; rows 0; date; statistics show -object lmgr_ng -counter files|hosts|owners|locks|*max -raw" >> statistics/$NODE_MANAGEMENT/locks.txt
 
 #Collect the statistics for vserver locks nfsv4
-ssh $USERNAME@$NODE_MANAGEMENT "set d -c off; rows 0; date; vserver locks nfsv4 show -inst" >> statistics/locks.txt
+ssh $USERNAME@$NODE_MANAGEMENT "set d -c off; rows 0; date; vserver locks nfsv4 show -inst" >> statistics/$NODE_MANAGEMENT/locks.txt
