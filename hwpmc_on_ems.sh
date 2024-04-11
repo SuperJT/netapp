9395634a65a9e3ae0018303c8e57c26403760ae9
Jason Townsend <jtownsen@netapp.com>
2024-04-11 15:43:11 -0500

    converted to match on a variable

--- hwpmc_on_ems.sh
+++ hwpmc_on_ems.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # ------------------------------------------------------------------
 # [Author: Jason Townsend] 
-#		   Monitor ems.log for bgp.vserver up and vserver down lines.  
+#		   Monitor ems.log for bgp.vserverDown down lines.  
 #          Once this finds a down event trigger collect profile
-#          Stop upon finding a bgp.vserver 
 #          Triggers EMS event tape.diagMsg when seen
 # ------------------------------------------------------------------
-# Instructions: (easiest way)
-# Place the script on a webserver
-# sudo nohup bash hwpmc_on_ems.sh
 
 
+## -- Notes for installing scripts.
 # curl -k0 https://raw.githubusercontent.com/SuperJT/netapp/master/hwpmc_on_ems.sh > hwpmc_on_ems.sh
+# sudo nohup bash hwpmc_on_ems.sh
 # curl -kO https://raw.githubusercontent.com/SuperJT/netapp/master/collectProfile-1.3-beta.sh
 # aff700s-2n-rtp-2::*> event generate -node local -message-name vifmgr.bgp.vserverDown jtown Default
 # aff700s-2n-rtp-2::*> event generate -node local -message-name vifmgr.bgp.vserverUp jtown Default
+
+emsMatch="vifmgr_bgp_vserverDown_1"
 
 # Download the script if it does not exist
 if [[ ! -f collectProfile-1.3-beta.sh ]]; then
@@ -28,11 +28,12 @@ fi
 echo "EMS Monitoring starting.."
 tail -f /mroot/etc/log/ems | while read -r line
 do
-    if [[ $line == *"vifmgr_bgp_vserverDown_1"* ]]; then
+    if [[ $line == *$emsMatch* ]]; then
         echo "vserverDown event detected, starting hwpmc script"
         ngsh -c "set d -c off;event generate -node local -message-name tape.diagMsg hwpmc-starting"
         bash collectProfile-1.3-beta.sh -d network &  # Start the hwpmc script in the background
-        ngsh -c "set d -c off;system node autosupport invoke -node local -message hwpmc_collected"
+        sleep 90 # sleep 90 seconds to let hwpmc finish collecting before triggering asup
+        ngsh -c "system node autosupport invoke -node local -message hwpmc_collected"
         echo "EMS Monitoring stopped.."
         exit 0
     fi
