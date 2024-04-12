b34146e3f90fb7aeafd851e3dfc488840dbb0c76
Jason Townsend <jtownsen@netapp.com>
2024-04-12 09:21:43 -0500

    updated follow option for tail

--- hwpmc_on_ems.sh
+++ hwpmc_on_ems.sh
@@ -9,7 +9,7 @@
 
 ## -- Notes for installing scripts.
 # curl -k0 https://raw.githubusercontent.com/SuperJT/netapp/master/hwpmc_on_ems.sh > hwpmc_on_ems.sh
-# sudo nohup bash hwpmc_on_ems.sh
+# sudo nohup bash hwpmc_on_ems.sh &
 # curl -kO https://raw.githubusercontent.com/SuperJT/netapp/master/collectProfile-1.3-beta.sh
 # aff700s-2n-rtp-2::*> event generate -node local -message-name vifmgr.bgp.vserverDown jtown Default
 # aff700s-2n-rtp-2::*> event generate -node local -message-name vifmgr.bgp.vserverUp jtown Default
@@ -26,14 +26,14 @@ if [[ ! -f collectProfile-1.3-beta.sh ]]; then
     fi
 fi
 echo "EMS Monitoring starting.."
-tail -f /mroot/etc/log/ems | while read -r line
+tail -F /mroot/etc/log/ems | while read -r line
 do
     if [[ $line == *$emsMatch* ]]; then
         echo "vserverDown event detected, starting hwpmc script"
         ngsh -c "set d -c off;event generate -node local -message-name tape.diagMsg hwpmc-starting"
         bash collectProfile-1.3-beta.sh -d network &  # Start the hwpmc script in the background
         sleep 90 # sleep 90 seconds to let hwpmc finish collecting before triggering asup
-        ngsh -c "system node autosupport invoke -node local -message hwpmc_collected"
+        ngsh -c "system node autosupport invoke -node -type all local -message hwpmc_collected"
         echo "EMS Monitoring stopped.."
         exit 0
     fi
