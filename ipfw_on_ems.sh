e137052a27d4772edd256ab844b529146e0b0c80
Jason Townsend <jtownsen@netapp.com>
2024-08-19 12:17:54 -0500

    ipfw changes

--- ipfw_on_ems.sh
+++ ipfw_on_ems.sh
@@ -2,7 +2,8 @@
 # ------------------------------------------------------------------
 # [Author: Jason Townsend] 
 #		   Monitor ems.log for ipfw.ReachedMaxStates lines.  
-#          Once this finds the event, trigger cleanup to tar files.
+#          Once this finds the event, trigger cleanup to tar files,
+#          generate an EMS event, and invoke AutoSupport.
 # ------------------------------------------------------------------
 
 # Directory where the output files will be saved
@@ -18,37 +19,27 @@ generate_ipfw_output() {
     local timestamp=$(date +"%Y%m%d-%H%M%S")
     local output_file="$output_dir/ipfw_dyn_list_$timestamp.txt"
     sudo ipfw -d list > "$output_file"
+    count_and_sort_ip_pairs "$output_file"
 }
 
-# Function to count and sort IP pairs from recent files and append the summary to each file
+# Function to count and sort IP pairs from a given file and append the summary to the file
 count_and_sort_ip_pairs() {
-    # Find files modified within the last 10 minutes
-    recent_files=$(find "$output_dir" -type f -name "ipfw_dyn_list_*.txt" -mmin -10)
-
-    # Check if there are any recent files
-    if [ -z "$recent_files" ]; then
-        echo "No recent files to process."
-        return
-    fi
-
-    # Process each recent file
-    for file in $recent_files; do
-        awk '
-        {
-            # Extract the IP pairs from the log lines
-            if (match($0, /([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+) [0-9]+ <-> ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/, arr)) {
-                ip_pair = arr[1] " <-> " arr[2]
-                ip_count[ip_pair]++
-            }
+    local file="$1"
+    awk '
+    {
+        # Extract the IP pairs from the log lines
+        if (match($0, /([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+) [0-9]+ <-> ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)/, arr)) {
+            ip_pair = arr[1] " <-> " arr[2]
+            ip_count[ip_pair]++
         }
-        END {
-            # Print the count of each IP pair
-            for (ip in ip_count) {
-                print ip_count[ip], ip
-            }
+    }
+    END {
+        # Print the count of each IP pair
+        for (ip in ip_count) {
+            print ip_count[ip], ip
         }
-        ' "$file" | sort -nr >> "$file"
-    done
+    }
+    ' "$file" | sort -nr | tee -a "$file"
 }
 
 # Function to clean up generated files by tarring them
@@ -62,7 +53,12 @@ cleanup() {
     else
         echo "No recent files to tar."
     fi
-    exit 0
+}
+
+# Function to trigger EMS event and generate AutoSupport
+trigger_asup() 
+    echo "Triggering ASUP for diagnosis"
+    ngsh -c "system node autosupport invoke -node local -type all -message IPFW_MAXSTATES_SCRIPT"
 }
 
 # Trap termination signals (SIGINT, SIGTERM) to perform cleanup
@@ -81,8 +77,8 @@ tail -F "$ems_log" | while read -r line
 do
     if [[ $line == *$emsMatch* ]]; then
         echo "ipfw.ReachedMaxStates event detected, starting cleanup"
-        count_and_sort_ip_pairs
         cleanup
+        trigger_asup
         echo "EMS Monitoring stopped.."
         exit 0
     fi
